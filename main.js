95cc065f46682706e0b3ce16fc653b3afc66eb95
Inosthful <inosthful@gmail.com>
2023-07-24 17:44:25 +0200

    commit

---
+++ main.js
@@ -0,0 +1,37 @@
+
+// let number = document.querySelector('#number');
+// number.addEventListener('keypress', function(e){
+  for (let i= 1; i <= 100; i++){
+
+    if( i % 15 == 0){
+      
+      console.log("FizzBuzz")
+      // document.write("FizzBuzz")
+    }
+    else if(i % 3 == 0){
+  
+      console.log("Fizz")
+      // document.write("Fizz")
+    }
+    else if(i % 5 == 0){
+    
+
+      console.log("Buzz")
+      // document.write("Buzz")
+    }
+    else{
+ 
+
+      console.log(i)
+      // document.write(i)
+      
+    }
+
+    }
+// });
+
+
+
+
+
+
